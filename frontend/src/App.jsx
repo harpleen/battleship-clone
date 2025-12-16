c40690695eec14511e3d25661502a7f95792a212
Imran Aderemi <121055300+ImranAderemi@users.noreply.github.com>
2025-12-16 14:28:22 +0000

    added a navbar, logo, and create-player page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './pages/Home'
+import Home from './pages/Home.jsx'
+import CreatePlayer from './pages/Create_Player.jsx'
+
 
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
@@ -8,7 +10,12 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/create-player',
+    element: <CreatePlayer />
   }
+
 ])
 
 function App() {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Navbar from '../components/Navbar.jsx'
+
 export default function Home() {
     return (
         <div>
+            <Navbar />
             <h1>Home</h1>
         </div>
     )
