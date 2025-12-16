9b16e396a552ddff49585f9217e75b97f6756184
Hafieza Begum <hafieza.begum@barclays.com>
2025-12-16 13:50:20 +0000

    Created the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import "./Home.css";
+
 export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  const handleStart = () => {
+    console.log("Start Game");
+  };
+
+  return (
+    <div className="crt">
+      <h1 className="title">WARHEADS</h1>
+      <p className="subtitle">COMBAT SIMULATOR</p>
+
+      <button className="start-btn" onClick={handleStart}>
+        ▶ Quick Game
+      </button>
+
+      <p className="footer">
+        © Shemaiah | Hafieza | Ismail | Daniel | Nazar | Imran
+      </p>
+    </div>
+  );
 }
