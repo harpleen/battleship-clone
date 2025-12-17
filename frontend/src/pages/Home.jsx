07cca451191a81d780d6d9b20f1b259387e3f382
Hafieza Begum <hafieza.begum@barclays.com>
2025-12-17 16:05:00 +0000

    routed the quick game button and changed the css on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,24 @@
 import "./Home.css";
-// import Navbar from "../components/Navbar.jsx";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
- <div className="crt">
-  <h1 className="title">WARHEADS</h1>
-  <p className="subtitle">NAVAL WAR SIMULATOR</p>
+    <div className="crt">
+      <h1 className="title">WARHEADS</h1>
+      <p className="subtitle">NAVAL WAR SIMULATOR</p>
 
-  <Link to="/create-player" className="start-btn">
-    ▶ Quick Game
-  </Link>
+      <Link to="/create-player" className="start-btn">
+        ▶ Quick Game
+      </Link>
 
-  {/* Radar pings */}
-  <div className="ping" style={{ top: "30%", left: "50%" }}></div>
-  <div className="ping" style={{ top: "70%", left: "60%" }}></div>
-  <div className="ping" style={{ top: "45%", left: "30%" }}></div>
+      {/* Radar pings */}
+      <div className="ping ring-1"></div>
+      <div className="ping ring-2"></div>
+      <div className="ping ring-3"></div>
 
-  <p className="footer">© Shemaiah | Hafieza | Ismail | Daniel | Nazar | Imran</p>
-</div>
-
+      <p className="footer">
+        © Shemaiah | Hafieza | Ismail | Daniel | Nazar | Imran
+      </p>
+    </div>
   );
 }
