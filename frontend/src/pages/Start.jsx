e9b7e8845418897f576bff110f6c04476d610c0e
Hafieza Begum <hafieza.begum@barclays.com>
2025-12-18 12:04:47 +0000

    fixed styling and routing of start page

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -3,39 +3,29 @@ import { Link } from 'react-router-dom';
 import './Start.css';
 
 const Start = () => {
-return (
-    <div className="crt">
-    
+  return (
+    <div className="start-crt">
+      
       {/* title */}
-    <h1 className="title">WARHEADS</h1>
-    <div className="subtitle">COMBAT SIMULATOR</div>
+      <h1 className="title">WARHEADS</h1>
+      <div className="subtitle">COMBAT SIMULATOR</div>
 
       {/* menu */}
-    <div className="menu">
+      <div className="menu">
         <Link to="/game" className="start-btn">
-        PLAY WITH ROBOT
+          START GAME
         </Link>
 
         <Link to="/" className="start-btn">
-        GAME MODE
+          GAME MODE
         </Link>
 
-        <Link to="/create-player" className="start-btn btn-back">
-        BACK TO MENU
+        <Link to="/" className="start-btn btn-back">
+          BACK TO MENU
         </Link>
+      </div>
     </div>
-
-      {/* Footer */}
-    <div className="footer">
-        <span>Shemaiah</span>
-        <span>Hafieza</span>
-        <span>Ismail</span>
-        <span>Daniel</span>
-        <span>Nazar</span>
-        <span>Imran</span>
-    </div>
-    </div>
-);
+  );
 };
 
 export default Start;
